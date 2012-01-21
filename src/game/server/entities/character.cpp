6493f33903c821e2ba6dbd03a53469faa3b09b9b
Teetime <TeetimeTW@yahoo.de>
2012-01-21 16:26:43 +0100

    removed healthmod indicator for instagib-gametypes

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -879,16 +879,19 @@ bool CCharacter::TakeDamage(vec2 Force, int Dmg, int From, int Weapon)
 
 	m_DamageTaken++;
 
-	// create healthmod indicator
-	if(Server()->Tick() < m_DamageTakenTick+25)
+	if(!GameServer()->m_pController->IsInstagib())
 	{
-		// make sure that the damage indicators doesn't group together
-		GameServer()->CreateDamageInd(m_Pos, m_DamageTaken*0.25f, Dmg);
-	}
-	else
-	{
-		m_DamageTaken = 0;
-		GameServer()->CreateDamageInd(m_Pos, 0, Dmg);
+		// create healthmod indicator
+		if(Server()->Tick() < m_DamageTakenTick+25)
+		{
+			// make sure that the damage indicators doesn't group together
+			GameServer()->CreateDamageInd(m_Pos, m_DamageTaken*0.25f, Dmg);
+		}
+		else
+		{
+			m_DamageTaken = 0;
+			GameServer()->CreateDamageInd(m_Pos, 0, Dmg);
+		}
 	}
 
 	if(Dmg)
