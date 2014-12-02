d12ad022062d08d7ab358f617f557c5fae0c5a0c
Teetime <TeetimeTW@yahoo.de>
2014-12-02 08:15:14 +0100

    0.7.5 release

--- src/game/version.h
+++ src/game/version.h
@@ -6,6 +6,6 @@
 #define GAME_VERSION "0.6.3"
 #define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH
 static const char GAME_RELEASE_VERSION[8] = {'0', '.', '6', '.', '2', 0};
-#define MOD_VERSION "0.7.5 BETA"
+#define MOD_VERSION "0.7.5"
 
 #endif
