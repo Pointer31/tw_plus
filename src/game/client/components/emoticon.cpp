4f0bebe0a34ca2c5d78e7c9b32a600271f1e880f
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2020-03-07 12:22:23 +0100

    Fix eyewheel

--- src/game/client/components/emoticon.cpp
+++ src/game/client/components/emoticon.cpp
@@ -172,12 +172,11 @@ void CEmoticon::OnRender()
 	Client()->GetServerInfo(&Info);
 	if(IsRace(&Info) && Config()->m_ClEyeWheel)
 	{
-		// broken alpha on this one...
-		/*Graphics()->TextureClear();
+		Graphics()->TextureClear();
 		Graphics()->QuadsBegin();
-		Graphics()->SetColor(1.0,1.0,1.0,0.3f);
+		Graphics()->SetColor(0.3f,0.3f,0.3f,0.3f);
 		DrawCircle(Screen.w/2, Screen.h/2, 100.0f, 64);
-		Graphics()->QuadsEnd();*/
+		Graphics()->QuadsEnd();
 
 		CTeeRenderInfo *pTeeInfo = &m_pClient->m_aClients[m_pClient->m_LocalClientID].m_RenderInfo;
 
