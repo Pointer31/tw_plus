971fe6609371c59a34a10ddbac7e464958e47097
Pointer31 <pointer.31.31.31@gmail.com>
2024-07-12 16:58:44 +0200

    Initial Code Upload

---
+++ compile.bat
@@ -0,0 +1,10 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+:nochmal
+..\bam\bam server_debug
+
+if %errorlevel% == 0 start "" tw+_srv_d.exe
+
+echo.
+set /p again=Again? 
+
+if "%again%" == "" goto nochmal
