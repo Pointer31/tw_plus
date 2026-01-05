02d2b01b072bb1e9e3abfb67db61e4d08e3b90c3
+KZ <60852359+M0REKZ@users.noreply.github.com>
2026-01-05 09:09:20 -0300

    I forgot flag

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -160,6 +160,8 @@ void CPlayer::Snap(int SnappingClient)
 		pPlayerInfo->m_PlayerFlags |= PLAYERFLAG_DEAD;
 	if(SnappingClient != -1 && (m_Team == TEAM_SPECTATORS || m_DeadSpecMode) && (SnappingClient == m_SpectatorID))
 		pPlayerInfo->m_PlayerFlags |= PLAYERFLAG_WATCHING;
+	if(m_pBotAI)
+		pPlayerInfo->m_PlayerFlags |= PLAYERFLAG_BOT;
 
 	pPlayerInfo->m_Latency = SnappingClient == -1 ? m_Latency.m_Min : GameServer()->m_apPlayers[SnappingClient]->m_aActLatency[m_ClientID];
 	pPlayerInfo->m_Score = m_Score;
