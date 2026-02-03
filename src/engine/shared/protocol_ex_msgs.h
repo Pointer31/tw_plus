8cd2b07e10cfe5dbbb9e23dda3c37f0949ae0397
+KZ <60852359+M0REKZ@users.noreply.github.com>
2026-02-03 20:04:49 -0300

    remove duplicated uuid

--- src/engine/shared/protocol_ex_msgs.h
+++ src/engine/shared/protocol_ex_msgs.h
@@ -20,7 +20,6 @@
 UUID(NETMSG_WHATIS,         "what-is@ddnet.tw")
 UUID(NETMSG_ITIS,           "it-is@ddnet.tw")
 UUID(NETMSG_IDONTKNOW,      "i-dont-know@ddnet.tw")
-UUID(NETMSG_MYOWNMESSAGE,   "my-own-message@heinrich5991.de")
 
 UUID(NETMSG_CLIENTVER, "clientver@ddnet.tw") //DDNet
 UUID(NETMSG_CLIENTVER_INFCLASS, "clientver@infclass") // InfClass
