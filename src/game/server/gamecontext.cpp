1b2d61d013d1c3f0b2dc492949e26d76af7e3278
Pointer31 <pointer.31.31.31@gmail.com>
2026-02-14 15:08:16 +0100

    fix: don't send invalid damage indicators

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -112,8 +112,8 @@ void CGameContext::CreateDamage(vec2 Pos, int Id, vec2 Source, int HealthAmount,
 		pEvent->m_Y = (int)Pos.y;
 		pEvent->m_ClientID = Id;
 		pEvent->m_Angle = (int)(f*256.0f);
-		pEvent->m_HealthAmount = HealthAmount;
-		pEvent->m_ArmorAmount = ArmorAmount;
+		pEvent->m_HealthAmount = minimum(HealthAmount, 9);
+		pEvent->m_ArmorAmount = minimum(ArmorAmount, 9);
 		pEvent->m_Self = Self;
 	}
 }
