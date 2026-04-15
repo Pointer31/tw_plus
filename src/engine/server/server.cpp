7668f9f6e54adc1ec172a940831b8ae680f5e4ef
Pointer31 <pointer.31.31.31@gmail.com>
2026-04-15 18:47:55 +0200

    sort of fix segfault issue

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -816,7 +816,10 @@ void CServer::UpdateClientRconCommands()
 			for(int i = 0; i < MAX_RCONCMD_SEND && m_aClients[ClientID].m_pRconCmdToSend; ++i)
 			{
 				SendRconCmdAdd(m_aClients[ClientID].m_pRconCmdToSend, ClientID);
-				m_aClients[ClientID].m_pRconCmdToSend = m_aClients[ClientID].m_pRconCmdToSend->NextCommandInfo(ConsoleAccessLevel, CFGFLAG_SERVER);
+				if (m_aClients[ClientID].m_pRconCmdToSend)
+					m_aClients[ClientID].m_pRconCmdToSend = m_aClients[ClientID].m_pRconCmdToSend->NextCommandInfo(ConsoleAccessLevel, CFGFLAG_SERVER);
+				else
+					Console()->Print(IConsole::OUTPUT_LEVEL_DEBUG, "server", "m_aClients[ClientID].m_pRconCmdToSend suddenly became null again");
 			}
 		}
 	}
